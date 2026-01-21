333d5a99ebfe4713ebcc4d896df37241f392bfb3
Aryan Chanda <aryan.chanda101@gmail.com>
2026-01-21 13:50:52 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const pageFlip = new St.PageFlip(
+  document.getElementById("flipbook"),
+  {
+    width: 600,
+    height: 800,
+
+    size: "stretch",
+    minWidth: 300,
+    maxWidth: 1200,
+    minHeight: 400,
+    maxHeight: 1600,
+
+    maxShadowOpacity: 0,   // removes page shadow
+    drawShadow: false,     // fully disable shadows
+
+    showCover: true,
+    mobileScrollSupport: false,
+
+    usePortrait: true,    // single page on mobile
+    autoSize: true
+  }
+);
+
+pageFlip.loadFromHTML(document.querySelectorAll(".page"));
